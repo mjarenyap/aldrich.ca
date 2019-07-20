387b9586411d90a3da64bc076ff726ca7b6a9995
mjarenyap <mjaren.yap@gmail.com>
2019-07-20 18:56:17 +0800

    Changed favicon

--- js/projects.js
+++ js/projects.js
@@ -8,25 +8,25 @@
 
 /* { Company Name : Image Name } */
 const companies = {
-	'Allovus': 'Allovus.png',
-	'Artefact': 'Artefact.png',
-	'BlackBerry': 'BlackBerry.png',
-	'Bloom Brothers': 'BloomBrothers.png',
-	'BMC Software': 'BMCSoftware.png',
-	'Digiteer': 'Digiteer.png',
-	'Enactus': 'Enactus.png',
-	'First Circle': 'FirstCircle.png',
-	'Founder Institute': 'FounderInstitute.png',
-	'Gates Foundation': 'GatesFoundation.png',
-	'Getty Images': 'GettyImages.png',
-	'IMS Health': 'IMSHealth.png',
-	'Insular Life': 'InsularLife.png',
-	'Remitly': 'Remitly.png',
-	'SFU': 'SFU.png',
-	'TMO1': 'TMO1.png',
-	'TMO2': 'TMO2.png',
-	'UXPH': 'UXPH.png',
-	'Weddingful': 'Weddingful.png'
+	'Allovus': ['Allovus.png', 'industry'],
+	'Artefact': ['Artefact.png', 'industry'],
+	'BlackBerry': ['BlackBerry.png', 'industry'],
+	'Bloom Brothers': ['BloomBrothers.png', 'industry'],
+	'BMC Software': ['BMCSoftware.png', 'industry'],
+	'Digiteer': ['Digiteer.png', 'industry'],
+	'Enactus': ['Enactus.png', 'industry'],
+	'First Circle': ['FirstCircle.png', 'industry'],
+	'Founder Institute': ['FounderInstitute.png', 'industry'],
+	'Gates Foundation': ['GatesFoundation.png', 'industry'],
+	'Getty Images': ['GettyImages.png', 'industry'],
+	'IMS Health': ['IMSHealth.png', 'industry'],
+	'Insular Life': ['InsularLife.png', 'industry'],
+	'Remitly': ['Remitly.png', 'industry'],
+	'SFU': ['SFU.png', 'industry'],
+	'TMO1': ['TMO1.png', 'industry'],
+	'TMO2': ['TMO2.png', 'industry'],
+	'UXPH': ['UXPH.png', 'industry'],
+	'Weddingful': ['Weddingful.png', 'industry']
 };
 
 $(document).ready(function() {
@@ -34,13 +34,13 @@ $(document).ready(function() {
 		$('#company-wrapper').append(`
 			<div class="company-card">
 				<!-- 1:1 ratio company logo/photo -->
-				<img src="assets/images/${x[1]}" class="featured-image" />
+				<img src="assets/images/${x[1][0]}" class="featured-image" />
 				<!-- Company name & industry -->
 				<div class="details">
 					<div align="center">
 						<h4 class="company-name">${x[0]}</h4>
 					</div>
-					<p class="industry">Industry</p>
+					<p class="industry">${x[1][1]}</p>
 				</div>
 			</div>
 			`);
